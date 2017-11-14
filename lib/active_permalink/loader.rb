b5f7034efb8fb46c6b95f8fad0967233b8e6ed30
Jonian Guveli <jonian@hardpixel.eu>
2017-11-14 15:12:52 +0200

    add has_permalink loader

---
+++ lib/active_permalink/loader.rb
@@ -0,0 +1,26 @@
+module ActivePermalink
+  module Loader
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def has_permalink(field, options={})
+        include ActiveDelegate
+        include Sluggable
+
+        assoc_opts = { as: :sluggable, class_name: 'ActivePermalink::Permalink', dependent: :destroy }
+        has_many :permalinks, assoc_opts
+        has_many :old_permalinks, -> { where active: false }, assoc_opts
+
+        has_one :active_permalink, -> { where active: true }, assoc_opts.merge(autosave: true)
+        delegate_attribute :slug, :string, to: :active_permalink
+
+        before_save do |record|
+          if slug.blank? or slug_changed?
+            premalink_generator   = Generator.new(record, field, slug_was, slug, options)
+            self.active_permalink = premalink_generator.new_permalink
+          end
+        end
+      end
+    end
+  end
+end
