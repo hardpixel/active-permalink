7ec22ce1260db79cc48781ce5fa8f5a8edad09c2
Jonian Guveli <jonian@hardpixel.eu>
2017-11-14 15:13:27 +0200

    update dependencies and autoload modules

--- lib/active_permalink.rb
+++ lib/active_permalink.rb
@@ -4,9 +4,27 @@ require 'babosa'
 require 'active_permalink/version'
 
 module ActivePermalink
-  class << self
-    def setup(&block)
-      yield
-    end
+  extend ActiveSupport::Autoload
+
+  # Autoload base modules
+  autoload :Config
+  autoload :Permalink
+  autoload :Generator
+  autoload :Sluggable
+  autoload :Loader
+
+  # Set attr accessors
+  mattr_accessor :config
+
+  # Set config options
+  @@config = {}
+
+  # Setup module
+  def self.setup
+    yield config
   end
+end
+
+ActiveSupport.on_load(:active_record) do
+  include ActivePermalink::Loader
 end
