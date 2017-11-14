65c193cbd68798c4901071e77843160f0a38c275
Jonian Guveli <jonian@hardpixel.eu>
2017-11-14 11:30:56 +0200

    update migration

--- lib/generators/active_permalink/templates/migration.rb
+++ lib/generators/active_permalink/templates/migration.rb
@@ -2,6 +2,7 @@ class CreatePermalinks < ActiveRecord::Migration[5.0]
   def change
     create_table :permalinks do |t|
       t.string  :scope,          default: 'global'
+      t.boolean :active,         default: true
       t.string  :slug,           null: false
       t.integer :sluggable_id,   null: false
       t.string  :sluggable_type, null: false
