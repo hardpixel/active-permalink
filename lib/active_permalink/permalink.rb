5911f4018b4c48c537fb4ac3bf17166fa530030c
Jonian Guveli <jonian@hardpixel.eu>
2020-05-01 23:07:41 +0300

    remove default order scope from permalink model

--- lib/active_permalink/permalink.rb
+++ lib/active_permalink/permalink.rb
@@ -2,8 +2,6 @@ module ActivePermalink
   class Permalink < ActiveRecord::Base
     self.table_name = 'permalinks'
 
-    default_scope { order created_at: :desc }
-
     scope :global,   -> { where scope: :global }
     scope :active,   -> { where active: true }
     scope :inactive, -> { where active: false }
